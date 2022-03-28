a7b5ed18c82ad837571fea53645b7b733069ad5a
Zenepe Kolaveri <zenepekolaveri@gmail.com>
2022-03-28 10:55:38 +0100

    First commit of lab

--- app/models/programme.js
+++ app/models/programme.js
@@ -17,18 +17,6 @@ class Programme {
         }
     }
 }
-async function getAllProgrammes() {
-    var sql = "SELECT * from Programmes"
-    const results = await db.query(sql);
-    var rows = [];
-    for (var row of results) {
-        // Use our Programme class to neatly format the object going to the template
-        rows.push(new Programme(row.id, row.name));
-    }
-    return rows;
-}
-
 module.exports = {
-    Programme,
-    getAllProgrammes,
+    Programme
 }
